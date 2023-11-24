5fb30ee5d0f1d1924acee609ff7442b6571365f0
Bruno Pinto <bruno.pinto.bp.20@gmail.com>
2023-11-24 01:27:12 +0000

    feat: ✨ added Processador class

---
+++ Habitacao/Componentes/Processador.cpp
@@ -0,0 +1,4 @@
+#include "Processador.h"
+
+namespace processador {
+} // processador
---
+++ Habitacao/Componentes/Processador.h
@@ -0,0 +1,12 @@
+#ifndef HOMESIM_PROCESSADOR_H
+#define HOMESIM_PROCESSADOR_H
+
+namespace processador {
+
+    class Processador {
+
+    };
+
+} // processador
+
+#endif //HOMESIM_PROCESSADOR_H
