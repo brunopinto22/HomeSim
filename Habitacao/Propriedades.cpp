ac4743093df397d3875e09352429870be4060335
Bruno Pinto <bruno.pinto.bp.20@gmail.com>
2023-11-24 01:26:02 +0000

    feat: ✨ added Propriedades class

---
+++ Habitacao/Propriedades.cpp
@@ -0,0 +1,4 @@
+#include "Propriedades.h"
+
+namespace propriedades {
+} // propriedades
---
+++ Habitacao/Propriedades.h
@@ -0,0 +1,12 @@
+#ifndef HOMESIM_PROPRIEDADES_H
+#define HOMESIM_PROPRIEDADES_H
+
+namespace propriedades {
+
+    class Propriedades {
+
+    };
+
+} // propriedades
+
+#endif //HOMESIM_PROPRIEDADES_H
