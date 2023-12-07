af7901d6cb2cfa6ebaab4a16439aefbdea296f8d
Bruno Pinto <bruno.pinto.bp.20@gmail.com>
2023-12-07 16:25:03 +0000

    feat: ✨ finished the classes for each Propriedade type

--- Habitacao/Propriedade.cpp
+++ Habitacao/Propriedade.cpp
@@ -1,37 +1,62 @@
-//#include <sstream>
-//#include "Propriedade.h"
-//
-//namespace propriedades {
-//    Propriedade::Propriedade() : value(UNSET) {}
-//
-//    int Propriedade::getValue() const { return value; }
-//
-//    std::string Propriedade::getUnit() const { return unit; }
-//
-//
-//    Propriedade Propriedade::operator+(int value) {
-//        this->value += value;
-//        return *this;
-//    }
-//
-//    Propriedade Propriedade::operator-(int value) {
-//        this->value -= value;
-//        return *this;
-//    }
-//
-//    Propriedade Propriedade::operator*(int value) {
-//        this->value *= value;
-//        return *this;
-//    }
-//
-//    Propriedade &Propriedade::operator=(int value) {
-//        this->value = value;
-//        return *this;
-//    }
-//
-//
-//
-//
-//
-//
-//} // propriedades
+#include "Propriedade.h"
+
+#include <utility>
+
+namespace propriedades {
+    Propriedade::Propriedade(std::string unit, int min, int max) : value(UNSET), unit(std::move(unit)), min(min), max(max) { }
+
+    int Propriedade::getValue() const { return value; }
+
+    std::string Propriedade::getUnit() const { return unit; }
+
+    bool Propriedade::checkNewValue(int &new_value) const {
+        if(max == UNSET)
+            return new_value >= min;
+
+        if(min == UNSET)
+            return new_value <= max;
+
+        return new_value >= min && new_value <=max;
+    }
+
+    Propriedade Propriedade::operator+(int new_value) {
+        if (checkNewValue(new_value))
+            this->value += new_value;
+        return *this;
+    }
+
+    Propriedade Propriedade::operator-(int new_value) {
+        if (checkNewValue(new_value))
+            this->value -= new_value;
+        return *this;
+    }
+
+    Propriedade Propriedade::operator*(int new_value) {
+        if (checkNewValue(new_value))
+            this->value *= new_value;
+        return *this;
+    }
+
+    Propriedade &Propriedade::operator=(int new_value) {
+        if (checkNewValue(new_value))
+            this->value = new_value;
+        return *this;
+    }
+
+
+    Temperature::Temperature() : Propriedade("ºC", -273, UNSET) {}
+
+    Light::Light() : Propriedade("Lumens", 0, UNSET) {}
+
+    Radiation::Radiation() : Propriedade("Becquerel", 0, UNSET) {}
+
+    Vibration::Vibration() : Propriedade("Hz", 0, UNSET) {}
+
+    Humidity::Humidity() : Propriedade("%", 0, 100) {}
+
+    Smoke::Smoke() : Propriedade("Obscuração (%)", 0, 100)  {}
+
+    Sound::Sound() : Propriedade("dB", 0, UNSET)  {}
+
+
+} // propriedades
--- Habitacao/Propriedade.h
+++ Habitacao/Propriedade.h
@@ -1,102 +1,81 @@
-//#ifndef HOMESIM_PROPRIEDADE_H
-//#define HOMESIM_PROPRIEDADE_H
-//
-//#include <string>
-//
-//namespace propriedades {
-//
-//    constexpr int UNSET = -1000;
-//
-//    class Propriedade {
-//    private:
-//        int value;
-//        std::string unit;
-//
-//    public:
-//        Propriedade();
-//        virtual ~Propriedade(){}
-//
-//        int getValue() const;
-//        // devolve o valor da propriedade
-//
-//        std::string getUnit() const;
-//        // devolve o valor da propriedade
-//
-//        virtual bool checkNewValue(int& new_value) const;
-//        // verifica se o valor novo e aceitavel para a propriedade
-//
-//        // funcoes aritmeticas
-//        Propriedade operator+(int value);
-//        Propriedade operator-(int value);
-//        Propriedade operator*(int value);
-//        Propriedade& operator=(int value);
-//
-//    };
-//
-//
-//    class Temperature : public Propriedade{
-//    public:
-//        bool checkNewValue(int& new_value) const override;
-//        Temperature operator+(int value);
-//        Temperature operator-(int value);
-//        Temperature operator*(int value);
-//        Temperature& operator=(int value);
-//    };
-//
-//    class Light : public Propriedade{
-//    public:
-//        bool checkNewValue(int& new_value) const override;
-//        Light operator+(int value);
-//        Light operator-(int value);
-//        Light operator*(int value);
-//        Light& operator=(int value);
-//    };
-//
-//    class Radiation : public Propriedade{
-//    public:
-//        bool checkNewValue(int& new_value) const override;
-//        Radiation operator+(int value);
-//        Radiation operator-(int value);
-//        Radiation operator*(int value);
-//        Radiation& operator=(int value);
-//    };
-//
-//    class Vibration : public Propriedade{
-//    public:
-//        bool checkNewValue(int& new_value) const override;
-//        Vibration operator+(int value);
-//        Vibration operator-(int value);
-//        Vibration operator*(int value);
-//        Vibration& operator=(int value);
-//    };
-//
-//    class Humidity : public Propriedade{
-//    public:
-//        bool checkNewValue(int& new_value) const override;
-//        Humidity operator+(int value);
-//        Humidity operator-(int value);
-//        Humidity operator*(int value);
-//        Humidity& operator=(int value);
-//    };
-//
-//    class Smoke : public Propriedade{
-//    public:
-//        bool checkNewValue(int& new_value) const override;
-//        Smoke operator+(int value);
-//        Smoke operator-(int value);
-//        Smoke operator*(int value);
-//        Smoke& operator=(int value);
-//    };
-//
-//    class Sound : public Propriedade{
-//    public:
-//        bool checkNewValue(int& new_value) const override;
-//        Sound operator+(int value);
-//        Sound operator-(int value);
-//        Sound operator*(int value);
-//        Sound& operator=(int value);
-//    };
-//
-//} // propriedades
-//
-//#endif //HOMESIM_PROPRIEDADE_H
+#ifndef HOMESIM_PROPRIEDADE_H
+#define HOMESIM_PROPRIEDADE_H
+
+#include <string>
+
+namespace propriedades {
+
+    constexpr int UNSET = -1000;
+
+    class Propriedade {
+    private:
+        // valor a que esta definido
+        int value;
+
+        // unidade de medida
+        std::string unit;
+
+        // valor max e min que a propriedade pode assumir
+        int min, max;
+
+    public:
+        Propriedade(std::string unit, int max, int min);
+        virtual ~Propriedade(){}
+
+        int getValue() const;
+        // devolve o valor da propriedade
+
+        std::string getUnit() const;
+        // devolve o valor da propriedade
+
+        bool checkNewValue(int& new_value) const;
+        // verifica se o valor novo e aceitavel para a propriedade
+
+        // funcoes aritmeticas
+        Propriedade operator+(int new_value);
+        Propriedade operator-(int new_value);
+        Propriedade operator*(int new_value);
+        Propriedade& operator=(int new_value);
+
+    };
+
+
+    //
+    class Temperature : public Propriedade{
+    public:
+        Temperature();
+    };
+
+    class Light : public Propriedade{
+    public:
+        Light();
+    };
+
+    class Radiation : public Propriedade{
+    public:
+        Radiation();
+    };
+
+    class Vibration : public Propriedade{
+    public:
+        Vibration();
+    };
+
+    class Humidity : public Propriedade{
+    public:
+        Humidity();
+    };
+
+    class Smoke : public Propriedade{
+    public:
+        Smoke();
+    };
+
+    class Sound : public Propriedade{
+    public:
+        Sound();
+    };
+
+} // propriedades
+
+#endif //HOMESIM_PROPRIEDADE_H
