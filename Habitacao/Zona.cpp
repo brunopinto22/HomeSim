08aeca185709c7dbb93b09104b11f4f80d1aeac6
Bruno Pinto <bruno.pinto.bp.20@gmail.com>
2023-12-07 16:26:10 +0000

    feat: ✨ added Propriedades to the Zona class

--- Habitacao/Zona.cpp
+++ Habitacao/Zona.cpp
@@ -3,6 +3,7 @@
 
 namespace zona {
     Zona::Zona(int number_id, int x, int y):id(number_id), pos_x(x), pos_y(y) {}
+    Zona::Zona() : id(-1) {}
 
     int Zona::getID() const { return id; }
 
@@ -10,11 +11,65 @@ namespace zona {
 
     int Zona::getPosY() const { return pos_y; }
 
-    std::string Zona::getProps() const {
+    int Zona::getTemperature() const { return temperature.getValue(); }
+    std::string Zona::getTemperatureStr() const {
         std::ostringstream oss;
-
+        oss << temperature.getValue() << temperature.getUnit();
 
         return oss.str();
     }
+
+    int Zona::getLight() const { return light.getValue(); }
+    std::string Zona::getLightStr() const {
+        std::ostringstream oss;
+        oss << light.getValue() << light.getUnit();
+
+        return oss.str();
+    }
+
+    int Zona::getRadiation() const { return radiation.getValue(); }
+    std::string Zona::getRadiationStr() const {
+        std::ostringstream oss;
+        oss << radiation.getValue() << radiation.getUnit();
+
+        return oss.str();
+    }
+
+    int Zona::getVibration() const { return vibration.getValue(); }
+    std::string Zona::getVibrationStr() const {
+        std::ostringstream oss;
+        oss << vibration.getValue() << vibration.getUnit();
+
+        return oss.str();
+    }
+
+    int Zona::getHumidity() const { return humidity.getValue(); }
+    std::string Zona::getHumidityStr() const {
+        std::ostringstream oss;
+        oss << humidity.getValue() << humidity.getUnit();
+
+        return oss.str();
+    }
+
+    int Zona::getSmoke() const { return smoke.getValue(); }
+    std::string Zona::getSmokeStr() const {
+        std::ostringstream oss;
+        oss << smoke.getValue() << smoke.getUnit();
+
+        return oss.str();
+    }
+
+    int Zona::getSound() const { return sound.getValue(); }
+    std::string Zona::getSoundStr() const {
+        std::ostringstream oss;
+        oss << sound.getValue() << sound.getUnit();
+
+        return oss.str();
+    }
+
+
+
+
+
 
 } // zona
--- Habitacao/Zona.h
+++ Habitacao/Zona.h
@@ -12,9 +12,16 @@ namespace zona {
         int id;
         int pos_x, pos_y; // coordenadas
 
-
+        propriedades::Temperature temperature;
+        propriedades::Light light;
+        propriedades::Radiation radiation;
+        propriedades::Vibration vibration;
+        propriedades::Humidity humidity;
+        propriedades::Smoke smoke;
+        propriedades::Sound sound;
 
     public:
+        Zona();
         Zona(int number_id, int x, int y);
 
         int getID() const;
@@ -25,9 +32,33 @@ namespace zona {
         int getPosY() const;
         // devolve a posicao em y
 
-        std::string getProps() const;
-        // devolve as Propriedades
+        int getTemperature() const;
+        std::string getTemperatureStr() const;
+        // devolve a temperatura
 
+        int getLight() const;
+        std::string getLightStr() const;
+        // devolve a luz
+
+        int getRadiation() const;
+        std::string getRadiationStr() const;
+        // devolve a radiacao
+
+        int getVibration() const;
+        std::string getVibrationStr() const;
+        // devolve a vibracao
+
+        int getHumidity() const;
+        std::string getHumidityStr() const;
+        // devolve a humidade
+
+        int getSmoke() const;
+        std::string getSmokeStr() const;
+        // devolve o fumo
+
+        int getSound() const;
+        std::string getSoundStr() const;
+        // devolve o som
 
     };
 
