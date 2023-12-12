71e1109c63338adb298b2eeb16faa7267aa0dd22
Bruno Pinto <bruno.pinto.bp.20@gmail.com>
2023-12-12 00:13:00 +0000

    feat: 🚧 added base constructors

--- Habitacao/Componentes/Aparelho.cpp
+++ Habitacao/Componentes/Aparelho.cpp
@@ -1,7 +1,7 @@
 #include "Aparelho.h"
 
 namespace aparelho {
-    Aparelho::Aparelho(int id, AparelhoType type) : componente::Componente(id, static_cast<char>(type)), isOn(false), type(type), ticks_passed(0)  {}
+    Aparelho::Aparelho(int id, AparelhoType type) : componente::Componente(id, static_cast<char>(type)), isOn(false), type(type), ticks_passed(0) {}
 
     bool Aparelho::getIsOn() const { return isOn; }
 
@@ -30,7 +30,7 @@ namespace aparelho {
         if(getIsOn() && getTicks() == 1){
 
             auto it = std::find_if(props.begin(), props.end(), [](propriedades::Propriedade* prop) {
-                return prop->getType() == "som";
+                return prop->getName() == "som";
             });
 
             if (it != props.end())
@@ -42,7 +42,7 @@ namespace aparelho {
         // primeiro instante desligado
         if(!getIsOn() && getTicks() == 1){
             auto it = std::find_if(props.begin(), props.end(), [](propriedades::Propriedade* prop) {
-                return prop->getType() == "som";
+                return prop->getName() == "som";
             });
 
             if (it != props.end())
@@ -53,7 +53,7 @@ namespace aparelho {
 
         if(getIsOn() && getTicks() % 3 == 0 && getTicks() <= 50){
             auto it = std::find_if(props.begin(), props.end(), [](propriedades::Propriedade* prop) {
-                return prop->getType() == "temperatura";
+                return prop->getName() == "temperatura";
             });
 
             if (it != props.end())
@@ -63,5 +63,23 @@ namespace aparelho {
 
     }
 
+
+    Aspersor::Aspersor(int id) : Aparelho(id, AparelhoType::ASPERSOR) { }
+
+    void Aspersor::run(std::vector<propriedades::Propriedade *> &props) {
+        Aparelho::run(props);
+    }
+
+    Refrigerador::Refrigerador(int id) : Aparelho(id, AparelhoType::REFRIGERADOR) { }
+
+    void Refrigerador::run(std::vector<propriedades::Propriedade *> &props) {
+        Aparelho::run(props);
+    }
+
+    Lampada::Lampada(int id) : Aparelho(id, AparelhoType::LAMPADA) { }
+
+    void Lampada::run(std::vector<propriedades::Propriedade *> &props) {
+        Aparelho::run(props);
+    }
 
 } // aparelho
