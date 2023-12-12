d04643ff98568f06a275d2621449c725c0c921a6
Bruno Pinto <bruno.pinto.bp.20@gmail.com>
2023-12-12 00:14:05 +0000

    feat: 🚧 base Sensor class
    created the base for the Sensor class
    
    the type doesnt get caught and so the object seems like its not being created

--- Habitacao/Componentes/Sensor.cpp
+++ Habitacao/Componentes/Sensor.cpp
@@ -1,4 +1,14 @@
 #include "Sensor.h"
 
 namespace sensor {
+    Sensor::Sensor(int id, propriedades::Propriedade &prop)
+    : componente::Componente(id, 't'), prop(prop) {}
+
+    char Sensor::getType() const {
+        return Componente::getType();
+    }
+
+    int Sensor::check() const { return prop.getValue(); }
+
+
 } // sensor
--- Habitacao/Componentes/Sensor.h
+++ Habitacao/Componentes/Sensor.h
@@ -1,9 +1,23 @@
 #ifndef HOMESIM_SENSOR_H
 #define HOMESIM_SENSOR_H
 
+#include "Componente.h"
+#include "../Propriedade.h"
+
 namespace sensor {
 
-    class Sensor {
+    class Sensor : public componente::Componente {
+    private:
+        propriedades::Propriedade& prop;
+
+    public:
+        Sensor(int id, propriedades::Propriedade &prop);
+
+        char getType() const override;
+        // devolve o caracter que o descreve
+
+        int check() const;
+        // verifica e devolve o valor da Proppriedade que esta a ver
 
     };
 
