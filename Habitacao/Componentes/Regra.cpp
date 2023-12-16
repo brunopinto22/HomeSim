21e48de4958aced65173ede5291b0273ac64343c
Bruno Pinto <bruno.pinto.bp.20@gmail.com>
2023-12-16 18:03:11 +0000

    feat: 🚧 created first base for the Regra class

--- Habitacao/Componentes/Regra.cpp
+++ Habitacao/Componentes/Regra.cpp
@@ -1,4 +1,36 @@
 #include "Regra.h"
 
 namespace regra {
+    Regra::Regra(RegraType type, sensor::Sensor& sensor) : type(type), sensor(sensor) {}
+
+    RegraType Regra::getType() const { return type; }
+
+    bool Regra::check() const { return false; }
+
+
+    RegraIgual::RegraIgual(sensor::Sensor& sensor, int value) : Regra(IGUAL, sensor), value(value) { }
+    bool RegraIgual::check() const {
+        return sensor.getValue() == value;
+    }
+
+    RegraMaior::RegraMaior(sensor::Sensor& sensor, int value) : Regra(MAIOR, sensor), value(value) { }
+    bool RegraMaior::check() const {
+        return sensor.getValue() > value;
+    }
+
+    RegraMenor::RegraMenor(sensor::Sensor& sensor, int value) : Regra(MENOR, sensor), value(value) { }
+    bool RegraMenor::check() const {
+        return sensor.getValue() < value;
+    }
+
+    RegraEntre::RegraEntre(sensor::Sensor& sensor, int value, int value2) : Regra(ENTRE, sensor), value(value), value2(value2) { }
+    bool RegraEntre::check() const {
+        return sensor.getValue() >= value && sensor.getValue() <= value2;
+    }
+
+    RegraFora::RegraFora(sensor::Sensor& sensor, int value, int value2) : Regra(FORA, sensor), value(value), value2(value2) { }
+    bool RegraFora::check() const {
+        return sensor.getValue() <= value && sensor.getValue() >= value2;
+    }
+
 } // regra
--- Habitacao/Componentes/Regra.h
+++ Habitacao/Componentes/Regra.h
@@ -1,11 +1,74 @@
 #ifndef HOMESIM_REGRA_H
 #define HOMESIM_REGRA_H
 
+#include "Sensor.h"
+
 namespace regra {
 
+    enum RegraType {
+        IGUAL,
+        MAIOR,
+        MENOR,
+        ENTRE,
+        FORA
+    };
+
     class Regra {
+        RegraType type;
+
+    protected:
+        sensor::Sensor& sensor;
+    public:
+        Regra(RegraType type, sensor::Sensor& sensor);
+
+        RegraType getType() const;
+        // devolve o tipo de Regra
+
+        virtual bool check() const;
+        // verifica se a regra e verdade
+        // retorna:     true - caso seja verdade
+        //              false - caso contrario
 
     };
+
+    class RegraIgual : public Regra{
+    private:
+        int value;
+    public:
+        RegraIgual(sensor::Sensor& sensor, int value);
+        bool check() const override;
+    };
+    class RegraMaior : public Regra{
+    private:
+        int value;
+    public:
+        RegraMaior(sensor::Sensor& sensor, int value);
+        bool check() const override;
+    };
+    class RegraMenor : public Regra{
+    private:
+        int value;
+    public:
+        RegraMenor(sensor::Sensor& sensor, int value);
+        bool check() const override;
+    };
+
+
+    class RegraEntre : public Regra{
+    private:
+        int value, value2;
+    public:
+        RegraEntre(sensor::Sensor& sensor, int value, int value2);
+        bool check() const override;
+    };
+    class RegraFora : public Regra{
+    private:
+        int value, value2;
+    public:
+        RegraFora(sensor::Sensor& sensor, int value, int value2);
+        bool check() const override;
+    };
+
 
 } // regra
 
